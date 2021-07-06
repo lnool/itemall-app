a5e606131c2c2fe21fad20ac8ad18a5941989771
didoee <2554771377@qq.com>
2021-07-06 11:38:21 +0800

    项目初始化

---
+++ src/components/TabBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab-bar">
+    <tab-bar-item
+      v-for="item in tabbar"
+      :key="item.id"
+      :path="item.path"
+      :activeColor="color"
+      class="tab-bar-item"
+    >
+      <i slot="icon" :class="['iconfont', item.icon]"></i>
+      <div slot="text">{{ item.text }}</div>
+    </tab-bar-item>
+  </div>
+</template>
+
+<script>
+import TabBarItem from "./TabBarItem.vue";
+export default {
+  components: { TabBarItem },
+  data() {
+    return {
+      tabbar: [
+        { id: 1, icon: "icon-iconfonthome0", text: "首页", path: "/" },
+        { id: 2, icon: "icon-fenlei", text: "分类", path: "/category" },
+        { id: 3, icon: "icon-shangcheng", text: "购物车", path: "/cart" },
+        { id: 4, icon: "icon-user", text: "我的", path: "/profile" },
+      ],
+      color: "#00BFC0",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.tab-bar {
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top: 1px solid #ddd;
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    padding: 3px 0;
+    i {
+      font-size: 18px;
+    }
+    div {
+      font-size: 14px;
+    }
+  }
+}
+</style>
---
+++ src/components/TabBarItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick" :style="active">
+    <slot name="icon"></slot>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 接收父组件传的数据
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red",
+    },
+  },
+  methods: {
+    itemClick() {
+      // 判断跳转的路由是否为活跃路由
+      if (this.$route.path !== this.path) this.$router.replace(this.path);
+    },
+  },
+  computed: {
+    active() {
+      return { color: this.$route.path === this.path ? this.activeColor : "#090C19" };
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
