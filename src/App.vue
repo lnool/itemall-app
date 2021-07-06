2e1530cf5bf4527231801328d58e769151f49dc9
didoee <2554771377@qq.com>
2021-07-06 11:40:37 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <router-view />
+   <tab-bar />
+  </div>
+</template>
+<script>
+import TabBar from './components/TabBar.vue'
+export default {
+  components:{
+    TabBar
+  }
+}
+</script>
+<style lang="less">
+@import url('./assets/font/iconfont.css');
+</style>
